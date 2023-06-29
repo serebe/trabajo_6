abddb39f38f298c12f0ae5fa450da2e769935231
Sebastian Restrepo <64997621+serebe@users.noreply.github.com>
2023-06-28 23:30:41 -0500

    solucion

---
+++ proyecto/src/main/java/last/Principal.java
@@ -0,0 +1,13 @@
+package last;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal UI = new VentanaPrincipal();
+        
+        UI.setVisible(true);
+        
+    }
+    
+}
